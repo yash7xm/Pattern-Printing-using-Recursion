d8fb0e918f8976e753e698cf2c46546f5b83688d
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2022-05-13 14:16:56 +0530

    Create 1st pattern.java

---
+++ 1st pattern.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		print(0,0,10);
+	}
+	static void print(int i, int j, int n){
+	    if(i>n) return;
+	    if(j<=i){
+	        System.out.print("* ");
+	        print(i,j+1,n);
+	    }
+	    else{
+	        System.out.println("");
+	        print(i+1,0,n);
+	    }
+	}
+}
