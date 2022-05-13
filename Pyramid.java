1bd74e7125d2b7b38c7f54c5c13ff44966de288b
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2022-05-13 15:14:12 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    print(0,0,0,3);
+	}
+	static void print(int i, int j, int s, int n){
+	   if(i>n) return;
+	   if(s<n-i){
+	       System.out.print("   ");
+	       print(i,j,s+1,n);
+	   }
+	   else if(j<2*i+1){
+	       System.out.print(" * ");
+	       print(i,j+1,s,n);
+	   }
+	   else{
+	       System.out.println("");
+	       print(i+1,0,0,n);
+	   }
+	}
+}
