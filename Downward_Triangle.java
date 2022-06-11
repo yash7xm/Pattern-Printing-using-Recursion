2177a837e941bdd2a8983a4f99f027b4f257a206
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2022-06-11 12:12:56 +0530

    Update Downward_Triangle.java

--- Downward_Triangle.java
+++ Downward_Triangle.java
@@ -2,17 +2,23 @@
 public class Main
 {
 	public static void main(String[] args) {
-		print(0,0,5);
+		
+		print(0, 0, 5);
+		
 	}
 	static void print(int i, int j, int n){
-	    if(i>n) return;
-	    if(j<=n-i){
+		
+	    if(i > n) return;
+		
+	    if(j <= n - i){
 	        System.out.print("* ");
-	        print(i,j+1,n);
+	        print(i, j + 1, n);
 	    }
+		
 	    else{
 	        System.out.println("");
-	        print(i+1,0,n);
+	        print(i + 1, 0, n);
 	    }
+		
 	}
 }
